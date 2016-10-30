b5298119db05ac7dc0beae1e0a5a76ac51021516
Chan <wailun@mac-chan.local>
2016-10-30 16:40:15 +0800

    used attribute list to represent y and class

--- mllib/DataReader.hpp
+++ mllib/DataReader.hpp
@@ -6,39 +6,44 @@
 
 namespace husky{
   namespace mllib{
-  husky::mllib::Instances tsvReader(std::string filepath){
-  husky::mllib::Instances instances();
-  std::mt19937 generator(std::time(0));
-  std::uniform_real_distribution<int> distribution(0, INT_MAX);
-  auto parser = [](boost::string_ref & chunk) {
-      if (chunk.size() == 0)
-        return;
+    husky::mllib::Instances tsvReader(std::string filepath){
+      husky::mllib::Instances instances();
+      std::mt19937 generator(std::time(0));
+      std::uniform_real_distribution<int> distribution(0, INT_MAX);
+      auto parser = [](boost::string_ref & chunk) {
+          if (chunk.size() == 0)
+            return;
 
-      // seperate the string
-      boost::char_separator<char> sep(" \t");
-      boost::tokenizer<boost::char_separator<char>> tok(chunk, sep);
-      husky::mllib:Instance instance;
-      instance.key = distribution(generator);
-      boost::tokenizer<boost::char_separator<char>>::iterator it = tok.begin();
-      while ((it+1) != tok.end()) {
-          instance.X.push_back(stod(*it++));
-      }
-      instance.last=*it;
-      instance.add(std::move(instance));
+          // seperate the string
+          boost::char_separator<char> sep(" \t");
+          boost::tokenizer<boost::char_separator<char>> tok(chunk, sep);
+          husky::mllib:Instance instance;
+          instance.key = distribution(generator);
+          boost::tokenizer<boost::char_separator<char>>::iterator it = tok.begin();
+          while ((it+1) != tok.end()) {
+              instance.X.push_back(stod(*it++));
+          }
+          instance.last=*it;
+          instances.add(std::move(instance));
 
-  };
-
-
-  husky::io::HDFSLineInputFormat infmt;
-  infmt.set_input(filepath);
-  husky::base::log_msg("started loading "+filepath);
-  husky::load(infmt, parser);
-  husky::base::log_msg("finished loading "+filepath);
-  instances.globalize();
-  husky::base::log_msg("finished globalizing instances");
+      };
 
 
+      husky::io::HDFSLineInputFormat infmt;
+      infmt.set_input(filepath);
+      husky::base::log_msg("start loading "+filepath);
+      husky::load(infmt, parser);
+      husky::base::log_msg("finished loading "+filepath);
+      instances.globalize();
+      husky::base::log_msg("finished globalizing instances");
+      husky::base::log_msg("start constructing y and class column");
+      list_execute(instances.enumerator(), [&instances](Instance& instance) {
+          instances.set_y(instance,std::stod(instance.last));
+          instances.set_class(instance,std::stoi(instance.last));
 
+      });
+      husky::base::log_msg("finished constructing y and class column");
+      return instances;
 
 }
 }
--- mllib/Instances.hpp
+++ mllib/Instances.hpp
@@ -15,8 +15,6 @@ public:
     virtual KeyT const & id() const { return key;}
 
     vec_double X;
-    double y;
-    int label;
     string last;
     Instance& operator=(const Instance& instance){
       if (this == &instance)
@@ -24,18 +22,16 @@ public:
       // do the copy
       key=instance.key;
       X=instance.X;
-      y=instance.y
-      label=instance.label;
       last=instance.last;
       // return the existing object so we can chain this operator
       return *this;
     }
     friend husky::BinStream& operator<<(husky::BinStream& stream, const Instance& u) {
-        stream << u.key << u.X << u.y << u.label << u.last;
+        stream << u.key << u.X << u.last;
         return stream;
     }
     friend husky::BinStream& operator>>(husky::BinStream& stream, Instance& u) {
-        stream >> u.key >> u.X >> u.y >> u.label >> u.last;
+        stream >> u.key >> u.X >> u.last;
         return stream;
     }
 };
@@ -43,17 +39,36 @@ public:
 class Instances{
 private:
   auto & list;
+  auto & ylist;
+  auto & classlist;
 public:
   int numAttributes;
   int numInstances;
+  int numClasses;
   Instances(){
     list=husky::ObjListFactory::create_objlist<Instance>();
+    ylist=list.create_attrlist<double>("y");
+    classlist=list.create_attrlist<int>("class");
+
   }
-  add(const Instance& instance){
+  void add(const Instance& instance){
     list.add_object(instance);
   }
-  globalize(){
+  void globalize(){
     husky::globalize(list);
+  }
+  void set_y(const Instance& instance,double y){
+    ylist.set(instance,y);
+  }
+  double get_y(const Instance& instance){
+    return ylist.get(instance);
+  }
+  void set_class(const Instance& instance,int label){
+    classlist.set(instance,label);
+  }
+  int get_class(const Instance& instance){
+    return classlist.get(instance);
+
   }
   auto& enumerator(){
     return list;
@@ -67,9 +82,13 @@ public:
     // do the copy
     numAttributes = instances.numAttributes;
     numInstances = instances.numInstances;
-    list_execute(instances.enumerator(), [](Instance& instance) {
+    numClasses = instances.numClasses;
+    list_execute(instances.enumerator(), [this,&instances](Instance& instance) {
         Instance copy=instance;
         add(copy);
+        set_y(copy,instances.get_y(instance));
+        set_class(copy,instances.get_class(instance))
+
     });
     husky::globalize(*this);
 
