d9285d669f44fa3c465426e255442961607f62b2
Chan <wailun_89@yahoo.com.hk>
2016-10-31 00:43:31 +0800

    tested functionality of Instances.hpp and tsvReader.hpp

--- examples/testcompiling.cpp
+++ examples/testcompiling.cpp
@@ -1,10 +1,61 @@
 
-#include "mllib/testcompile.hpp"
+#include <mllib/Instances.hpp>
+#include <mllib/DataReader.hpp>
+
+std::string vec_to_str(vec_double v) {
+    std::string str("");
+    for (auto& x : v) {
+        str += std::to_string(x);
+        str += " ";
+    }
+    return str;
+}
+
+
+
+
+
+void testcompiling(){
+  using namespace husky;
+  mllib::Instances instances;
+  husky::mllib::tsvReader(instances,"/1155032497/mycpu2.tsv");
+  base::log_msg("checking X y and class,key");
+  list_execute(instances.enumerator(), [&instances](mllib::Instance& instance) {
+    std::vector<double> here=instance.X;
+
+    here.push_back(instances.get_y(instance));
+    here.push_back(instances.get_class(instance));
+    here.push_back(instance.key);
+    base::log_msg(vec_to_str(here));
+
+  });
+  base::log_msg("...");
+  base::log_msg("...");
+  base::log_msg("...");
+  base::log_msg("...");
+  base::log_msg("checking load balancing");
+  base::log_msg(std::to_string(instances.enumerator().get_size()));
+  base::log_msg("...");
+  base::log_msg("...");
+  base::log_msg("...");
+  base::log_msg("...");
+  base::log_msg("checking statistics");
+  base::log_msg("numClasses: "+std::to_string(instances.numClasses));
+  base::log_msg("numAttributes: "+std::to_string(instances.numAttributes));
+  base::log_msg("numInstances: "+std::to_string(instances.numInstances));
+
+
+}
+
+
 
 
 int main(int argc, char** argv) {
-    if (husky::init_with_args(argc, argv)) {
-        husky::run_job(pi);
+    std::vector<std::string> args;
+    args.push_back("hdfs_namenode");
+    args.push_back("hdfs_namenode_port");
+    if (husky::init_with_args(argc, argv,args)) {
+        husky::run_job(testcompiling);
         return 0;
     }
     return 1;
--- mllib/DataReader.hpp
+++ mllib/DataReader.hpp
@@ -4,16 +4,19 @@
 #include "io/input/hdfs_line_inputformat.hpp"
 #include "boost/tokenizer.hpp"
 #include <stdexcept>
+#include "lib/aggregator_factory.hpp"
 
 namespace husky{
   namespace mllib{
-    husky::mllib::Instances tsvReader(std::string filepath){
-      husky::mllib::Instances instances;
+    husky::mllib::Instances& tsvReader(husky::mllib::Instances& instances,std::string filepath){
       int total_num_workers=husky::Context::get_worker_info()->get_num_workers();
       int key_giver=((int)INT_MAX/total_num_workers) * husky::Context::get_global_tid();
 
-
-      auto parser = [&instances,&key_giver](boost::string_ref & chunk) {
+      husky::lib::Aggregator<int> total_num_examples(0, [](int & a, const int & b) { a += b;});
+      husky::lib::Aggregator<int> num_features(0, [](int & a, const int & b) { if(a==0)a = b;else if(a==b) return; else throw std::runtime_error( "input data error: the dimensionality" );});
+      husky::lib::Aggregator<int> num_classes(0, [](int & a, const int & b) { a= std::max(a,b);});
+      auto& ac = husky::lib::AggregatorFactory::get_channel();
+      auto parser = [&instances,&key_giver,&total_num_examples,&num_features](boost::string_ref & chunk) {
           if (chunk.size() == 0)
             return;
 
@@ -29,6 +32,7 @@ namespace husky{
               it++;
           }
           it=tok.begin();
+          num_features.update(length-1);
           while (length >1){
             instance.X.push_back(std::stod(*it++));
             length--;
@@ -36,23 +40,29 @@ namespace husky{
           }
           instance.last=*it;
           instances.add(std::move(instance));
+          total_num_examples.update(1);
+
 
       };
 
-
+      husky::lib::AggregatorFactory::sync();
       husky::io::HDFSLineInputFormat infmt;
       infmt.set_input(filepath);
       husky::base::log_msg("start loading "+filepath);
-      husky::load(infmt, parser);
+      husky::load(infmt, {&ac},parser);
       husky::base::log_msg("finished loading "+filepath);
       instances.globalize();
       husky::base::log_msg("finished globalizing instances");
       husky::base::log_msg("start constructing y and class column");
-      list_execute(instances.enumerator(), [&instances](Instance& instance) {
+      list_execute(instances.enumerator(), {},{&ac},[&instances,&num_classes](Instance& instance) {
           instances.set_y(instance,std::stod(instance.last));
           instances.set_class(instance,std::stoi(instance.last));
+          num_classes.update(std::stoi(instance.last)+1);
 
       });
+      instances.numClasses=num_classes.get_value();
+      instances.numAttributes=num_features.get_value();
+      instances.numInstances=total_num_examples.get_value();
       husky::base::log_msg("finished constructing y and class column");
       return instances;
 
