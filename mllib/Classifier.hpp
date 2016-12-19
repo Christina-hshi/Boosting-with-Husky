1128f7f0c5fc79619279c769105b0307007e4233
RickyChanWL <wailun_89@yahoo.com.hk>
2016-12-19 18:18:01 +0800

    changed function name

--- mllib/Classifier.hpp
+++ mllib/Classifier.hpp
@@ -6,11 +6,11 @@ namespace husky{
       class Classifier{
           public:
               virtual void fit(const Instances& instances) = 0;
-              virtual AttrList<Instance, double>&  predict(Instances& instances,int classnow,std::string prediction_name="predicted_probability")=0;
+              virtual AttrList<Instance, double>&  predict_proba(Instances& instances,int classnow,std::string prediction_name="predicted_probability")=0;
               // using the default method to convert probability to output label
-              virtual AttrList<Instance, int>&  predict_label(Instances& instances,std::string prediction_name="predicted_label")=0;
+              virtual AttrList<Instance, int>&  classify(Instances& instances,std::string prediction_name="predicted_label")=0;
               // the clone function will only copy hyperparameters but not estimated parameter
-              virtual Estimator* clone(int seed=0)=0;
+              virtual Classifier* clone(int seed=0)=0;
       };
 }
 }
