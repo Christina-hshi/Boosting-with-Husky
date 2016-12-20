eaad3f9add37ea46bfc671daac9943a02d5afb81
Christina-hshi <hshi@cse.cuhk.edu.hk>
2016-12-21 00:10:54 +0800

    add fit with weight in Classifier

--- mllib/Classifier.hpp
+++ mllib/Classifier.hpp
@@ -21,7 +21,8 @@ namespace husky{
       class Classifier{
           public:
               virtual void fit(const Instances& instances) = 0;
-              virtual AttrList<Instance, Prediction>&  predict(Instances& instances,std::string prediction_name="prediction")=0;
+              virtual void fit(const Instances& instances, std::string instance_weight_name) = 0;
+              virtual AttrList<Instance, Prediction>&  predict(const Instances& instances,std::string prediction_name="prediction")=0;
               // the clone function will only copy hyperparameters but not estimated parameter
               virtual Classifier* clone(int seed=0)=0;
       };
