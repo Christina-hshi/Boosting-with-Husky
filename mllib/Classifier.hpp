d84a3434eeef51edfeb91748834a7b168de7f2e4
Christina-hshi <hshi@cse.cuhk.edu.hk>
2016-12-20 21:37:59 +0800

    update classifier

--- mllib/Classifier.hpp
+++ mllib/Classifier.hpp
@@ -1,14 +1,19 @@
 #pragma once
-#include <mllib/Instances.hpp>
+#include "mllib/Instances.hpp"
 
 namespace husky{
   namespace mllib{
+      class Prediction{
+      public:
+          int label;
+          vec_double proba;
+          
+          explicit Prediction(int l, vec_double p): label(l), proba(p){}
+      };
       class Classifier{
           public:
               virtual void fit(const Instances& instances) = 0;
-              virtual AttrList<Instance, double>&  predict_proba(Instances& instances,int classnow,std::string prediction_name="predicted_probability")=0;
-              // using the default method to convert probability to output label
-              virtual AttrList<Instance, int>&  classify(Instances& instances,std::string prediction_name="predicted_label")=0;
+              virtual AttrList<Instance, Prediction>&  predict(Instances& instances,std::string prediction_name="prediction")=0;
               // the clone function will only copy hyperparameters but not estimated parameter
               virtual Classifier* clone(int seed=0)=0;
       };
